3402f31b9274c6f130e629efedc83e615a4b2463
Liucongcong <liu_congcong@icloud.com>
2024-08-29 18:05:40 +0800

    Update threadPool.c

--- threadPool.c
+++ threadPool.c
@@ -1,94 +1,84 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <assert.h>
 #include "threadPool.h"
-
-static int function(void *arg)
-{
-    return 0;
-}
 
 static void *threadPoolWorker(void *threadPool)
 {
     ThreadPool *x = (ThreadPool *)threadPool;
     while (1)
     {
-        assert(!pthread_mutex_lock(&x->mutex));
+        pthread_mutex_lock(&x->mutex);
         while (!x->nTasks && !x->flag)
         {
-            assert(!pthread_cond_wait(&x->cond, &x->mutex));
+            pthread_cond_wait(&x->cond, &x->mutex);
         }
         if (x->nTasks > 0)
         {
             Task *task = x->tasks;
             x->tasks = task->next;
             x->nTasks--;
-            assert(!pthread_mutex_unlock(&x->mutex));
-            assert(!(*task->function)(task->arg));
+            pthread_mutex_unlock(&x->mutex);
+            (*task->function)(task->arg);
             free(task);
         }
         else if (x->flag)
         {
-            assert(!pthread_mutex_unlock(&x->mutex));
+            pthread_mutex_unlock(&x->mutex);
             pthread_exit(NULL);
         }
     }
     return NULL;
 }
 
-ThreadPool* threadPoolCreate(int nThreads)
+ThreadPool *threadPoolCreate(int nThreads)
 {
     ThreadPool *threadPool = malloc(sizeof(ThreadPool));
-    assert(threadPool);
     threadPool->nTasks = 0;
     threadPool->flag = 0;
-    assert(!pthread_mutex_init(&threadPool->mutex, NULL));
-    assert(!pthread_cond_init(&threadPool->cond, NULL));
+    pthread_mutex_init(&threadPool->mutex, NULL);
+    pthread_cond_init(&threadPool->cond, NULL);
     threadPool->threads = malloc(sizeof(pthread_t) * nThreads);
-    assert(threadPool->threads);
     threadPool->tasks = NULL;
     threadPool->nThreads = nThreads;
-    assert(threadPool->threads);
     for (int i = 0; i < nThreads; i++)
     {
-        assert(!pthread_create(&threadPool->threads[i], NULL, threadPoolWorker, threadPool));
+        pthread_create(&threadPool->threads[i], NULL, threadPoolWorker, threadPool);
     }
     return threadPool;
 }
 
 int threadPoolPut(ThreadPool *threadPool, int (*function)(void *), void *arg)
 {
-    while (threadPool->nTasks >= 10 * threadPool->nThreads)
+    while (threadPool->nTasks > threadPool->nThreads)
     {
         sleep(1);
     }
-    assert(!pthread_mutex_lock(&threadPool->mutex));
     Task *task = malloc(sizeof(Task));
-    assert(task);
     task->function = function;
     task->arg = arg;
+    pthread_mutex_lock(&threadPool->mutex);
     task->next = threadPool->tasks;
     threadPool->tasks = task;
     threadPool->nTasks++;
-    assert(!pthread_cond_signal(&threadPool->cond));
-    assert(!pthread_mutex_unlock(&threadPool->mutex));
+    pthread_cond_signal(&threadPool->cond);
+    pthread_mutex_unlock(&threadPool->mutex);
     return 0;
 }
 
-int threadPoolFree(ThreadPool *pool)
+int threadPoolFree(ThreadPool *threadPool)
 {
-    assert(!pthread_mutex_lock(&pool->mutex));
-    pool->flag = 1;
-    assert(!pthread_cond_broadcast(&pool->cond));
-    assert(!pthread_mutex_unlock(&pool->mutex));
-    for (int i = 0; i < pool->nThreads; i++)
+    pthread_mutex_lock(&threadPool->mutex);
+    threadPool->flag = 1;
+    pthread_cond_broadcast(&threadPool->cond);
+    pthread_mutex_unlock(&threadPool->mutex);
+    for (int i = 0; i < threadPool->nThreads; i++)
     {
-        assert(!pthread_join(pool->threads[i], NULL));
+        pthread_join(threadPool->threads[i], NULL);
     }
-    assert(!pthread_mutex_destroy(&pool->mutex));
-    assert(!pthread_cond_destroy(&pool->cond));
-    free(pool->threads);
-    free(pool);
+    pthread_mutex_destroy(&threadPool->mutex);
+    pthread_cond_destroy(&threadPool->cond);
+    free(threadPool->threads);
+    free(threadPool);
     return 0;
 }
